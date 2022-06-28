85d91db49960e46e7a6d25f63837ca240087d5b3
Srimal Priyanga <s.priyanga22@gmail.com>
2022-06-28 14:15:40 +0530

    all project works includin source

---
+++ Source/AdpiyasaApp/www/js/myscript.js
@@ -0,0 +1,12 @@
+/* 
+ * Copyright © 2012 - 2013 D2Real Solutions.
+ * All Rights Reserved.
+ * 
+ * These materials are unpublished, proprietary, confidential source code of
+ * D2Real Solutions (pvt) Limited and constitute a TRADE SECRET of D2Real Solutions (pvt) Limited.
+ * 
+ * Creater : Srimal Priyanga < s.priyanga22@gmail.com >
+ * Created on : Sep 12, 2014, 6:53:34 PM
+ */
+
+
